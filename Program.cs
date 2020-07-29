98f4fa43933406da438a3023a40d840f9b976c36
marty <martyna.sacharczuk@gmail.com>
2020-07-29 17:35:56 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace slonie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( Slonie.func());
+            Console.ReadKey();
+            
+
+        }
+       
+    }
+    class Slonie
+    {
+        // n, wagi sloni, obecne ustawienie, ustawienie szefa
+        static int maxN = 1000000;
+        static int inf = 1000000000;
+
+        static int[] weight = new int[maxN];
+        static int[] currentOrd = new int[maxN];
+        static int[] finalOrd = new int[maxN];
+        static bool[] visited = new bool[maxN];
+
+        static int minWeight = inf;
+       public static long func() {
+            int N = 0;
+            try
+            {
+                 N = int.Parse(Console.ReadLine());
+                weight = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+                currentOrd = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)-1).ToArray();
+                // finalOrd = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+                int[] tab = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)-1).ToArray();
+                finalOrd = new int[N];
+                for (int i = 0; i < tab.Length; i++)
+                    finalOrd[tab[i]] = currentOrd[i];
+                for (int i = 0; i < weight.Length; i++)
+                {
+                    minWeight = Math.Min(minWeight, weight[i]);
+                }
+
+                if (weight.Length != N || currentOrd.Length != N || finalOrd.Length != N) throw new Exception();
+
+            }
+            catch (Exception e) { }
+
+
+            long result = 0;
+      
+            for (int i = 0; i<N;++i)
+            {
+                if(!visited[i])
+                {
+                    int minWeightC = inf;
+                    long sum = 0;
+                    int current = i;
+                    int lenghtC = 0;
+                    for(; ; )
+                    {
+                        minWeightC = Math.Min(minWeightC, weight[current]);
+                        sum += weight[current];
+                        current = finalOrd[current];
+                        visited[current] = true;
+                        ++lenghtC;
+                        if (current == i) break;
+                    }
+                    result += Math.Min(sum + (lenghtC - 2) * (long)minWeightC, sum + minWeightC + (lenghtC + 1) * (long)minWeight);
+                }
+            }
+            return result;
+           
+        }
+
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("slonie")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("slonie")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("96f8a5a3-8835-45aa-85c0-0a0c5689d579")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
